202c6b79496ef96313379ab0c6a05b062f26fd1f
Md Shadhin <shadhin12580@gmail.com>
2023-07-12 17:59:47 +0600

    aa

--- cpt/projects/tpl/each-item-skin-style-current-theme5.php
+++ cpt/projects/tpl/each-item-skin-style-current-theme5.php
@@ -1,20 +1,26 @@
+<!-- Project Block -->
 <div class="type-projects projects-current-theme5">
-  <div class="inner-box">
-    <a href="<?php the_permalink();?>" class="image-box">
-      <?php echo get_the_post_thumbnail( get_the_ID(), $feature_thumb_image_size, array( 'class' => 'img-fullwidth' ) );?>
-    </a>
-    <div class="info-box">
-      <?php if ( $show_title == 'yes' ) : ?>
-        <<?php echo esc_attr( $title_tag );?> class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></<?php echo esc_attr( $title_tag );?>>
-      <?php endif; ?>
+	<div class="inner-box">
+		<div class="image">
+			<?php echo get_the_post_thumbnail( get_the_ID(), $feature_thumb_image_size, array( 'class' => 'img-fullwidth' ) );?>
+		</div>
+		<div class="info-box">
+			<?php if ( $show_title == 'yes' ) : ?>
+				<<?php echo esc_attr( $title_tag );?> class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></<?php echo esc_attr( $title_tag );?>>
+			<?php endif; ?>
 
-      <?php mascot_core_digicod_get_cpt_shortcode_template_part( 'part-excerpt', null, 'projects/tpl', $settings, false );?>
-
-      <?php if ( $show_cat == 'yes' ) : ?>
-        <ul class="cat-list">
-          <?php include('term-list-each-post.php'); ?>
-        </ul>
-      <?php endif; ?>
-    </div>
-  </div>
+			<?php if ( $show_view_details_button == 'yes' ) : ?>
+				<?php mascot_core_digicod_get_cpt_shortcode_template_part( 'button', null, 'projects/tpl', $settings, false ); ?>
+			<?php endif; ?>
+		</div>
+		<?php if ( $show_cat == 'yes' ) : ?>
+		<ul class="cat-list">
+			<?php include('term-list-each-post.php'); ?>
+		</ul>
+		<?php endif; ?>
+	</div>
 </div>
+
+
+
+
