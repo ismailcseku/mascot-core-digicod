3b0544493f59298e13af1e3c5c2ac0e2db65c1cc
zakariakodesolutiongit <83698891+zakariakodesolutiongit@users.noreply.github.com>
2023-07-12 15:30:13 +0600

    flaticon

--- widgets/service-block/tpl/service-item-skin-style2.php
+++ widgets/service-block/tpl/service-item-skin-style2.php
@@ -1,12 +1,14 @@
 <!-- Service Block Style2-->
 <?php $service_item['settings'] = $settings; ?>
 <div class="service-block-style2">
-  <?php mascot_core_digicod_get_shortcode_template_part( 'icon-type', $service_item['icon_type'], 'service-block/tpl', $service_item, false );?>
-  <div class="details">
-    <?php mascot_core_digicod_get_shortcode_template_part( 'part-title', null, 'service-block/tpl', $service_item, false );?>
-    <?php mascot_core_digicod_get_shortcode_template_part( 'part-content', null, 'service-block/tpl', $service_item, false );?>
-    <?php if ( $show_view_details_button == 'yes' ) : ?>
-      <?php mascot_core_digicod_get_shortcode_template_part( 'button', null, 'service-block/tpl', $service_item, false );?>
-    <?php endif; ?>
+  <div class="inner-box">
+    <div class="image">
+      <?php mascot_core_digicod_get_shortcode_template_part( 'part-featured-image', null, 'service-block/tpl', $service_item, false );?>
+    </div>
+    <div class="content-box">
+      <?php mascot_core_digicod_get_shortcode_template_part( 'icon-type', $service_item['icon_type'], 'service-block/tpl', $service_item, false );?>
+      <?php mascot_core_digicod_get_shortcode_template_part( 'part-subtitle', null, 'service-block/tpl', $service_item, false );?>
+      <?php mascot_core_digicod_get_shortcode_template_part( 'part-title', null, 'service-block/tpl', $service_item, false );?>
+    </div>
   </div>
 </div>
