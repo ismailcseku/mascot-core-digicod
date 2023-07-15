8289acf18363a2785127eec0699d26140c563b13
Md Shadhin <shadhin12580@gmail.com>
2023-07-15 11:22:41 +0600

    aaa

--- cpt/projects/tpl/each-item-skin-style-current-theme11.php
+++ cpt/projects/tpl/each-item-skin-style-current-theme11.php
@@ -1,5 +1,5 @@
 <!-- Project Block -->
-<div class="type-projects projects-current-theme1">
+<div class="type-projects projects-current-theme11">
 	<div class="inner-box">
 		<div class="image">
 			<?php echo get_the_post_thumbnail( get_the_ID(), $feature_thumb_image_size, array( 'class' => 'img-fullwidth' ) );?>
@@ -7,24 +7,6 @@
 		<div class="info-box">
 			<?php if ( $show_title == 'yes' ) : ?>
 				<<?php echo esc_attr( $title_tag );?> class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></<?php echo esc_attr( $title_tag );?>>
-			<?php endif; ?>
-
-			<?php if ( $show_excerpt == 'yes' ) : ?>
-				<?php if ( empty($excerpt_length) ) { ?>
-					<?php $excerpt = get_the_excerpt(); ?>
-					<?php if ( !empty($excerpt) ) { ?>
-						<div class="excerpt">
-							<?php echo esc_html( strip_shortcodes( get_the_excerpt() ) )?>
-						</div>
-					<?php } ?>
-				<?php } else { ?>
-					<?php $excerpt = get_the_excerpt(); ?>
-					<?php if ( !empty($excerpt) ) { ?>
-						<div class="excerpt">
-							<?php echo esc_html( digicod_slice_excerpt_by_length( $excerpt, $excerpt_length ) ); ?>
-						</div>
-					<?php } ?>
-				<?php } ?>
 			<?php endif; ?>
 		</div>
 		<?php if ( $show_cat == 'yes' ) : ?>
--- cpt/projects/tpl/each-item-skin-style-current-theme12.php
+++ cpt/projects/tpl/each-item-skin-style-current-theme12.php
@@ -1,5 +1,5 @@
 <!-- Project Block -->
-<div class="type-projects projects-current-theme1">
+<div class="type-projects projects-current-theme12">
 	<div class="inner-box">
 		<div class="image">
 			<?php echo get_the_post_thumbnail( get_the_ID(), $feature_thumb_image_size, array( 'class' => 'img-fullwidth' ) );?>
@@ -9,23 +9,7 @@
 				<<?php echo esc_attr( $title_tag );?> class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></<?php echo esc_attr( $title_tag );?>>
 			<?php endif; ?>
 
-			<?php if ( $show_excerpt == 'yes' ) : ?>
-				<?php if ( empty($excerpt_length) ) { ?>
-					<?php $excerpt = get_the_excerpt(); ?>
-					<?php if ( !empty($excerpt) ) { ?>
-						<div class="excerpt">
-							<?php echo esc_html( strip_shortcodes( get_the_excerpt() ) )?>
-						</div>
-					<?php } ?>
-				<?php } else { ?>
-					<?php $excerpt = get_the_excerpt(); ?>
-					<?php if ( !empty($excerpt) ) { ?>
-						<div class="excerpt">
-							<?php echo esc_html( digicod_slice_excerpt_by_length( $excerpt, $excerpt_length ) ); ?>
-						</div>
-					<?php } ?>
-				<?php } ?>
-			<?php endif; ?>
+			<a href="<?php the_permalink();?>" class="read-more"><i class="icon fas fa-angle-double-right"></i></a>
 		</div>
 		<?php if ( $show_cat == 'yes' ) : ?>
 		<ul class="cat-list">
