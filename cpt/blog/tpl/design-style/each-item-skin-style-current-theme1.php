f9a70a760c65d762acc1417f42c60328c1eb0122
Md Shadhin <shadhin12580@gmail.com>
2023-07-15 13:20:17 +0600

    AAA

--- cpt/blog/tpl/design-style/each-item-skin-style-current-theme1.php
+++ cpt/blog/tpl/design-style/each-item-skin-style-current-theme1.php
@@ -1,59 +1,35 @@
 <!-- News Block Style1 -->
 <div class="blog-item-current-style1">
-  <?php if ( $show_featured_image == 'yes' ) : ?>
-  <div class="entry-header">
-    <?php digicod_get_post_thumbnail( $post_format, $featured_image_size ); ?>
-  </div>
-  <?php endif; ?>
-  <div class="entry-content">
-    <?php if ( $show_post_meta == 'yes' ) : ?>
-      <?php
-      $post_meta_options_array = explode(',', $post_meta_options);
-      if ( in_array( $show_post_meta_over_featured_image, $post_meta_options_array ) ) {
-        ?>
-        <div class="post-single-meta">
-          <?php digicod_post_shortcode_single_meta( $show_post_meta_over_featured_image ); ?>
+    <?php if ( $show_featured_image == 'yes' ) : ?>
+      <div class="entry-header">
+        <?php digicod_get_post_thumbnail( $post_format, $featured_image_size ); ?>
+        <?php digicod_post_category();?>
+      </div>
+    <?php endif; ?>
+    <div class="entry-content">
+
+      <?php if ( $show_post_meta == 'yes' ) : ?>
+        <?php digicod_post_shortcode_meta( $post_meta_options, array( $show_post_meta_over_featured_image ) ); ?>
+      <?php endif; ?>
+      <?php if ( $show_title == 'yes' ) : ?>
+        <div class="title-holder">
+          <?php the_title( '<'.esc_attr( $title_tag ).' class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></'.esc_attr( $title_tag ).'>' ); ?>
         </div>
-        <?php
-      }
-      ?>
-    <?php endif; ?>
-    <?php if ( $show_title == 'yes' ) : ?>
-      <div class="title-holder">
-        <?php the_title( '<'.esc_attr( $title_tag ).' class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></'.esc_attr( $title_tag ).'>' ); ?>
-      </div>
-    <?php endif; ?>
-    <?php if ( $show_excerpt == 'yes' ) : ?>
-      <div class="post-excerpt">
-        <?php if ( empty($excerpt_length) ) { ?>
-          <?php digicod_get_excerpt(); ?>
-        <?php } else { ?>
-          <?php digicod_get_excerpt($excerpt_length); ?>
-        <?php } ?>
-      </div>
-    <?php endif; ?>
-    <?php if ( $show_view_details_button == 'yes' ) : ?>
-			<?php mascot_core_digicod_get_cpt_shortcode_template_part( 'button', null, 'blog/tpl/post-format', $settings, false );?>
-    <?php endif; ?>
-  </div>
+      <?php endif; ?>
+      <?php if ( $show_excerpt == 'yes' ) : ?>
+        <div class="post-excerpt">
+          <?php if ( empty($excerpt_length) ) { ?>
+            <?php digicod_get_excerpt(); ?>
+          <?php } else { ?>
+            <?php digicod_get_excerpt($excerpt_length); ?>
+          <?php } ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ( $show_view_details_button == 'yes' ) : ?>
+        <?php mascot_core_digicod_get_cpt_shortcode_template_part( 'button', null, 'blog/tpl/post-format', $settings, false );?>
+      <?php endif; ?>
+
+    </div>
   <div class="clearfix"></div>
-</div>
-
-
-
-<div class="news-block">
-  <div class="inner-box">
-    <div class="image-box">
-      <figure class="image"><a href="news-details.html"><img src="images/resource/news1-1.jpg" alt=""></a></figure>
-      <span class="cat">Evaulation</span>
-    </div>
-    <div class="content-box">
-      <ul class="post-info">
-        <li><i class="fas fa-calendar-alt"></i> 01 January 2023</li>
-      </ul>
-      <h4 class="title"><a href="news-details.html">Your online success is our priority amplify your digital presence</a></h4>
-      <div class="text">Our mission is there are variations passages of Lorem Ipsum available, but the majority have suffered alteration</div>
-      <a href="news-details.html" class="theme-btn btn-style-one read-more"><span class="btn-title">Read More <i class="fa fa-arrow-right"></i></span></a>
-    </div>
-  </div>
 </div>
