71df5609c6d6500075dd3d6cb9e70983d5b6b434
zakariakodesolutiongit <83698891+zakariakodesolutiongit@users.noreply.github.com>
2023-07-15 13:15:40 +0600

    team6

--- widgets/team-block/tpl/team-item-skin-style4.php
+++ widgets/team-block/tpl/team-item-skin-style4.php
@@ -6,12 +6,12 @@
       <div class="image">
         <?php mascot_core_digicod_get_shortcode_template_part( 'part-thumb', null, 'team-block/tpl', $team_item, false );?>
       </div>
+      <?php mascot_core_digicod_get_shortcode_template_part( 'part-social-links', null, 'team-block/tpl', $team_item, false );?>
     </div>
     <div class="info-box">
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-title', null, 'team-block/tpl', $team_item, false );?>
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-subtitle', null, 'team-block/tpl', $team_item, false );?>
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-short-bio', null, 'team-block/tpl', $team_item, false );?>
-      <?php mascot_core_digicod_get_shortcode_template_part( 'part-social-links', null, 'team-block/tpl', $team_item, false );?>
     </div>
   </div>
 </div>
--- widgets/team-block/tpl/team-item-skin-style6.php
+++ widgets/team-block/tpl/team-item-skin-style6.php
@@ -6,9 +6,10 @@
       <div class="image">
         <?php mascot_core_digicod_get_shortcode_template_part( 'part-thumb', null, 'team-block/tpl', $team_item, false );?>
       </div>
+      <?php mascot_core_digicod_get_shortcode_template_part( 'part-social-links', null, 'team-block/tpl', $team_item, false );?>
+      <span class="share-icon fa fa-share-alt"></span>
     </div>
     <div class="info-box">
-      <?php mascot_core_digicod_get_shortcode_template_part( 'part-social-links', null, 'team-block/tpl', $team_item, false );?>
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-title', null, 'team-block/tpl', $team_item, false );?>
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-subtitle', null, 'team-block/tpl', $team_item, false );?>
       <?php mascot_core_digicod_get_shortcode_template_part( 'part-short-bio', null, 'team-block/tpl', $team_item, false );?>
