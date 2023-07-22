ce40b6f75821005b115b573728bfffb0ab159472
MD ISMAIL HOSSAIN <ismailcseku@gmail.com>
2023-07-22 19:52:17 +0600

    s

--- widgets/testimonial-block/tpl/part-thumb.php
+++ widgets/testimonial-block/tpl/part-thumb.php
@@ -1,1 +1,1 @@
-<img src="<?php $image = wp_get_attachment_image_src( $feature_image['id'], $feature_image_size); echo esc_url( $image[0] );?>" alt="<?php esc_attr_e( 'Image', 'mascot-core-digicod'); ?>">
+<img src="<?php $image = wp_get_attachment_image_src( $feature_image['id'], $feature_image_size); echo esc_url( $image[0] );?>" width="<?php echo esc_attr( $image[1] );?>" height="<?php echo esc_attr( $image[2] );?>" alt="<?php esc_attr_e( 'Image', 'mascot-core-digicod'); ?>">
