c4d06fcf92308de1012f2693f160b223a499b644
MD ISMAIL HOSSAIN <ismailcseku@gmail.com>
2023-07-22 19:50:55 +0600

    Update icon-type-image.php

--- widgets/service-block/tpl/icon-type-image.php
+++ widgets/service-block/tpl/icon-type-image.php
@@ -1,3 +1,3 @@
 <div class="service-icon icon-img">
-	<img src="<?php $image = wp_get_attachment_image_src( $service_icon_image['id'], $service_icon_image_size); echo esc_url( $image[0] );?>" alt="<?php esc_attr_e( 'Image', 'mascot-core-digicod'); ?>">
+	<img src="<?php $image = wp_get_attachment_image_src( $service_icon_image['id'], $service_icon_image_size); echo esc_url( $image[0] );?>" width="<?php echo esc_attr( $image[1] );?>" height="<?php echo esc_attr( $image[2] );?>" alt="<?php esc_attr_e( 'Image', 'mascot-core-digicod'); ?>">
 </div>
