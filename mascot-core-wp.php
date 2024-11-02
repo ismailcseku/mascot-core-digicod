bb68b3fe7ceba25352c7c320ef24c65f26c03834
Nazmus Sakib <32157125+Sakib62r@users.noreply.github.com>
2024-11-02 15:19:45 +0600

    a

--- mascot-core-wp.php
+++ mascot-core-wp.php
@@ -4,7 +4,7 @@
 Plugin Name: Mascot Core - Digicod
 Plugin URI:  https://themeforest.net/user/thememascot/portfolio
 Description: Mascot Core Plugin for Elementor. It includes all the required Shortcodes needed by Elementor.
-Version:     1.0
+Version:     2.0
 Author:      ThemeMascot
 Author URI:  https://themeforest.net/user/thememascot/portfolio
 Text Domain: mascot-core-digicod
